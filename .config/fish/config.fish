9a1311d2a04f371a07adeead5537eba7de8db1b7
Lusbert <mazinany.armin@gmail.com>
2025-01-01 18:50:31 +0330

    added rm function but with trashcan in /tmp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     alias bat="bat --style=numbers --color=always"
     alias zed=zeditor
     alias date="date \"+%Y-%m-%d %A %T %B UTC%Z\""
-    set -Ux LESS '--ignore-case --quiet --long-prompt --wordwrap --no-vbell --use-color'
+    set -Ux LESS '--ignore-case --incsearch --search-options=W --quiet --long-prompt --wordwrap --no-vbell --use-color'
 
     abbr -a -- mkdir  mkdir -p
     abbr -a -- ls     ls -Alh --color=always
@@ -18,6 +18,20 @@ if status is-interactive
     fish_add_path /home/lusbert/.juliaup/bin/
 
     # i have no idea what to call this part
+    # this func is probably risky and unsecure, but shrug imma just comment it out, cause why not
+#    function remove_file
+#        mkdir -p /tmp/trashcan
+#        if test (count (ls /tmp/trashcan/)) -gt 0
+#            echo "Cleaning up trashcan..."
+#            rm -r /tmp/trashcan/*
+#        end    
+#        if test -n "$argv"
+#            mv $argv /tmp/trashcan/
+#            echo "Moved $argv to trashcan."
+#        else
+#            echo "Error: No file specified."
+#        end
+#    end
     set EDITOR nvim
     function fish_greeting
         fastfetch -c ~/.config/fastfetch/small.jsonc
