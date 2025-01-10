d40a65097b4f0c9d774d62f41702791f617fdc6b
Lusbert <mazinany.armin@gmail.com>
2025-01-10 08:06:22 +0330

    ignored qview config
    why the hell does it keep a recents section in a fricking config file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,11 @@ if status is-interactive
     set -Ux LESS '--ignore-case --incsearch --search-options=W --quiet --long-prompt --wordwrap --no-vbell --use-color'
 
     abbr -a -- mkdir  mkdir -p
-    abbr -a -- ls     ls -Alh --color=always
+    abbr -a -- ls     ls -Alh --si --color=always
     abbr -a -- rg     rg --hidden -i
+    abbr -a -- du     du -sh --si
+    abbr -a -- ncdu   ncdu --color dark --si
     abbr -a -- tree   tree -l -C
-    abbr -a -- du     du -sh
     abbr -a -- sudo     doas
     abbr -a -- sudoedit doasedit
 
@@ -18,20 +19,6 @@ if status is-interactive
     fish_add_path /home/lusbert/.juliaup/bin/
 
     # i have no idea what to call this part
-    # this func is probably risky and unsecure, but shrug imma just comment it out, cause why not
-#    function remove_file
-#        mkdir -p /tmp/trashcan
-#        if test (count (ls /tmp/trashcan/)) -gt 0
-#            echo "Cleaning up trashcan..."
-#            rm -r /tmp/trashcan/*
-#        end    
-#        if test -n "$argv"
-#            mv $argv /tmp/trashcan/
-#            echo "Moved $argv to trashcan."
-#        else
-#            echo "Error: No file specified."
-#        end
-#    end
     set EDITOR nvim
     function fish_greeting
         fastfetch -c ~/.config/fastfetch/small.jsonc
